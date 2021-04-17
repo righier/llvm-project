a98bc7ae7f4177d1d3cacab52af5123e4e1729b2
Michael Kruse <llvm-project@meinersbur.de>
2021-04-16 23:06:29 -0500

    GOMP not gomp

--- clang/test/TransformPragma/noopt.c
+++ clang/test/TransformPragma/noopt.c
@@ -2,25 +2,18 @@
 // RUN: %clang_cc1 -flegacy-pass-manager -triple x86_64-pc-windows-msvc19.0.24215 -std=c99 -fno-unroll-loops -O3 -mllvm -polly -mllvm -polly-position=early -mllvm -polly-process-unprofitable -mllvm -polly-reschedule=0 -mllvm -polly-pattern-matching-based-opts=0 -mllvm -polly-use-llvm-names -emit-llvm -o - %s | FileCheck --check-prefix=TRANS %s
 
 __attribute__((noinline))
-void matmul(int M, int N, int K, double C[const restrict static M][N], double A[const restrict static M][K], double B[const restrict static K][N]) {
-  #pragma clang loop id(i)
+void loopnest(int M, int N, double C[const restrict static M][N]) {
   for (int i = 0; i < M; i += 1)
-    #pragma clang loop id(j)
     for (int j = 0; j < N; j += 1)
-      #pragma clang loop id(k)
-      for (int k = 0; k < K; k += 1)
-        C[i][j] += A[i][k] * B[k][j];
+        C[i][j] = i + j;
 }
 
 
-// AST:      // Loop: i
+// AST:      // anon loop
 // AST-NEXT: for (int c0 = 0; c0 < M; c0 += 1) {
-// AST-NEXT:   // Loop: j
-// AST-NEXT:   for (int c1 = 0; c1 < N; c1 += 1) {
-// AST-NEXT:     // Loop: k
-// AST-NEXT:     for (int c2 = 0; c2 < K; c2 += 1)
-// AST-NEXT:       Stmt_for_body8(c0, c1, c2);
-// AST-NEXT:   }
+// AST-NEXT:   // anon loop
+// AST-NEXT:   for (int c1 = 0; c1 < N; c1 += 1) 
+// AST-NEXT:     Stmt_for_body4(c0, c1);
 // AST-NEXT: }
 
 
