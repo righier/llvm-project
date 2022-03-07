0d113e53d0821e0383354c6a568aa040fc8e6e1b
Ermanno Righini <ermannorighini@gmail.com>
2022-03-07 20:38:34 +0100

    edited build script

--- llvm-build.sh
+++ llvm-build.sh
@@ -5,23 +5,21 @@ mkdir -p ./build-install
 
 pushd ./build
 
-pwd
-
 cmake -G Ninja \
-	-DCMAKE_C_COMPILER=clang-13 \
-	-DCMAKE_CXX_COMPILER=clang++-13 \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
 	-DCMAKE_CXX_STANDARD=14 \
 	-DCMAKE_INSTALL_PREFIX=/home/ermanno/thesis/llvm-project/build-install \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLLVM_ENABLE_ASSERTIONS=ON \
-	-DLLVM_USE_LINKER=lld-13 \
+	-DLLVM_USE_LINKER=lld \
 	-DLLVM_TARGETS_TO_BUILD=X86 \
 	-DLLVM_ENABLE_PROJECTS='clang;polly' \
 	../llvm
 
 cmake --build .
 
-cmake --build . --target check-polly
+#cmake --build . --target check-polly
 
 cmake --install .
 
