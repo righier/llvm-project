150e08446f8079088a85064a941fa1aab7bb3937
Ermanno Righini <ermannorighini@gmail.com>
2023-03-15 06:16:16 +0100

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 BASE_DIR=$(dirname $(realpath $BASH_SOURCE))
 
@@ -8,18 +8,16 @@ mkdir -p ./build-install
 pushd ./build
 
 cmake -G Ninja \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
 	-DCMAKE_CXX_STANDARD=14 \
 	-DCMAKE_INSTALL_PREFIX=$BASE_DIR/build-install \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLLVM_ENABLE_ASSERTIONS=ON \
-	-DLLVM_USE_LINKER=lld \
+	-DLLVM_USE_LINKER=gold \
 	-DLLVM_TARGETS_TO_BUILD=X86 \
 	-DLLVM_ENABLE_PROJECTS='clang;polly' \
 	../llvm
 
-cmake --build .
+cmake --build . --config Release -- -j 8
 
 cmake --install .
 
